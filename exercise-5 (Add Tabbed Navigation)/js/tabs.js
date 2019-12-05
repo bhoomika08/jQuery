b440a9b4bcbf7bff5475aafc66edb6c5d552986f
Bhoomika <bhoomika@vinsol.com>
2019-12-05 12:18:27 +0530

    proper indentation

--- exercise-5 (Add Tabbed Navigation)/js/tabs.js
+++ exercise-5 (Add Tabbed Navigation)/js/tabs.js
@@ -4,48 +4,48 @@ class TabbedNavigation {
     this.unorderedList = $('<ul></ul>');
 	}
 
-	init() {
-		this.hideAll(this.allModules);
-		this.createUnorderedList();
-		this.addListItemToUnorderedList();
-		this.bindEvents();
-		this.showFirstTab();
-	}
+  init() {
+    this.hideAll(this.allModules);
+    this.createUnorderedList();
+    this.addListItemToUnorderedList();
+    this.bindEvents();
+    this.showFirstTab();
+  }
 
 //1. Hide all of the modules.
 	hideAll(elements) {
-		elements.hide();
-	}
+    elements.hide();
+  }
 
 //2. Create an unordered list element before the first module.
-	createUnorderedList() {
+  createUnorderedList() {
     $('.module:first').before(this.unorderedList);
-	}
+  }
 
 //3. Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the unordered list element.
 	addListItemToUnorderedList() {
-		this.allModules.each((index, value) => {
-			var $listItemText = $(value).find('h2').text();
-		  var $listItem = $('<li>' + $listItemText + '</li>');
-		  this.unorderedList.append($listItem);
-		});
-	}
+    this.allModules.each((index, value) => {
+      var $listItemText = $(value).find('h2').text();
+      var $listItem = $('<li>' + $listItemText + '</li>');
+      this.unorderedList.append($listItem);
+    });
+  }
 
 //4. Bind a click event to the list item
   bindEvents() {
-  	let _this = this;
-  	this.unorderedList.find('li').bind({
-  		'click': function() {
-  			_this.bindClickEventToListItem(this);
-  		} 
-  	});
+    let _this = this;
+    this.unorderedList.find('li').bind({
+      'click': function() {
+        _this.bindClickEventToListItem(this);
+      }
+    });
   }
 
   bindClickEventToListItem(listItem) {
-  	let tab = $("div#" + $(listItem).text().toLowerCase());
+    let tab = $("div#" + $(listItem).text().toLowerCase());
     this.allModules.hide();
     tab.show();
-    this.unorderedList.children().removeClass("current")
+    this.unorderedList.children().removeClass("current");
     $(listItem).addClass("current");
   }
 
@@ -56,4 +56,4 @@ class TabbedNavigation {
   }
 }
 
-new TabbedNavigation().init(); 
+new TabbedNavigation().init();
